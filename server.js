1e9fbc397ffa274905cfc59fa70e3e193270a788
fd <fede.atanasoff@gmail.com>
2018-07-30 01:29:01 -0300

    configurando express y caperta publica

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  //   res.send("hellooooo");
-  let fede = {
-    nombre: "fede",
-    alias: "thexdecline",
-    url: req.url
-  };
-
-  res.send(fede);
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, () => console.log("escuchando peticiones en el puerto 3000"));
