0dde18a7c3edb07444d3af996ed2ce00e1364de9
fd <fede.atanasoff@gmail.com>
2018-07-31 13:13:35 -0300

    agregando hbs y eliminando las vistas anteriores

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+// MIDDLEWARES
 app.use(express.static(__dirname + "/public"));
+
+// HANDLEBARS
+app.set("view engine", "hbs");
+
+app.get("/", (requestAnimationFrame, res) => {
+  res.render("home", {
+    saludo: "helloooooo",
+    anio: new Date().getFullYear()
+  });
+});
 
 app.listen(3000, () => console.log("escuchando peticiones en el puerto 3000"));
